ac622ba47d49afeb31793d6ff6081b017187456a
alkainno <alka.chauhan@innotechteam@gmail.com>
2023-09-11 18:23:05 +0530

    made the changes

--- script.js
+++ script.js
@@ -1,97 +1,90 @@
-let valueOfInput, description, input, temp, longitudeData, latitudeData;
+let valueOfInput,
+  description,
+  temp,
+  longitudeData,
+  latitudeData,
+  iconCode,
+  fetchedData;
 console.log("hello");
 const apiKey = "82005d27a116c2880c8f0fcb866998a0";
-const api = () => {
-  valueOfInput = document.getElementById("inputData").value;
-  console.log("inpuuuuuuuuuut", valueOfInput);
-  document.getElementById("icon").src = "loader.gif";
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${valueOfInput}&appid=82005d27a116c2880c8f0fcb866998a0`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      input = data;
-      document.getElementById("display").style.display = "block";
-      document.getElementById("error").style.display = "none";
-      console.log("alpha ddddd====>", input);
-      icons(input);
-      let t = parseFloat(input.main.temp - 273).toFixed(2);
 
-      document.getElementById("temp").innerHTML = t + " °C";
-      if (input.weather[0].description.includes("rain")) {
-        console.log("-------rainging-----");
-        document.getElementById("display").style.backgroundImage =
-          "url('icons/rain.jpg')";
-      } else if (input.weather[0].description.includes("cloud")) {
-        console.log("-------clouds-----");
-        document.getElementById("display").style.backgroundImage =
-          "url('icons/cloud.jpg')";
-      } else if (input.weather[0].description.includes("sky")) {
-        console.log("-------clouds-----");
-        document.getElementById("display").style.backgroundImage =
-          "url('icons/sunny.jpeg')";
-      } else {
-        document.getElementById("display").style.backgroundImage = "none";
-      }
-
-      document.getElementById("description").innerHTML =
-        input.weather[0].description;
-      document.getElementById("city-country").innerHTML =
-        input.name + " - " + input.sys.country;
-
-      document.getElementById("description").innerHTML =
-        input.weather[0].description;
-    })
-    .catch((err) => {
-      console.log(err);
-      document.getElementById("display").style.display = "none";
-      document.getElementById("error").style.display = "block";
-    });
-};
-const icons = (temp) => {
-  document.getElementById("icon").src = "loader.gif";
-  let iconCode = temp.weather[0].icon;
-  console.log("iconcode---->", iconCode);
-  let iconNumber = `icons/${iconCode}.png`;
-  document.getElementById("icon").src = iconNumber;
+const apiData = async (data) => {
+  let url =
+    typeof data === "string"
+      ? `https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${apiKey}`
+      : `https://api.openweathermap.org/data/2.5/weather?lat=${data[0]}&lon=${data[1]}&appid=${apiKey}`;
+  const response = await fetch(url);
+  const fetchedData = await response.json();
+  return fetchedData;
 };
 
-function getCordinates() {
+const api = async (data) => {
+  try {
+    valueOfInput = document.getElementById("inputData").value;
+    const data = await apiData(valueOfInput);
+    document.getElementById("icon").src = "loader.gif";
+    document.getElementById("display").style.display = "block";
+    document.getElementById("error").style.display = "none";
+    console.log("alpha ddddd====>", data);
+    let celciusTemperature = parseFloat(data.main.temp - 273).toFixed(2);
+    document.getElementById("temp").innerHTML = celciusTemperature + " °C";
+
+    document.getElementById("icon").src = "loader.gif";
+    iconCode = data.weather[0].icon;
+    document.getElementById("icon").src = `icons/${iconCode}.png`;
+
+    description = data.weather[0].description;
+    description.includes("rain")
+      ? (document.getElementById("display").style.backgroundImage =
+          "url('icons/rain.jpg')")
+      : description.includes("cloud")
+      ? (document.getElementById("display").style.backgroundImage =
+          "url('icons/cloud.jpg')")
+      : description.includes("sky")
+      ? (document.getElementById("display").style.backgroundImage =
+          "url('icons/sunny.jpeg')")
+      : (document.getElementById("display").style.backgroundImage = "none");
+
+    document.getElementById("description").innerHTML = description;
+    document.getElementById("city-country").innerHTML =
+      data.name + " - " + data.sys.country;
+  } catch (err) {
+    console.log(err);
+    document.getElementById("display").style.display = "none";
+    document.getElementById("error").style.display = "block";
+  }
+};
+
+const getCordinates = () => {
+  let locationArray = [];
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (location) => {
-        latitudeData = location.coords.latitude;
-        longitudeData = location.coords.longitude;
-        fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${latitudeData}&lon=${longitudeData}&appid=82005d27a116c2880c8f0fcb866998a0`
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            temp = data;
-            console.log("val of temp--->", temp);
-            icons(temp);
-            let celciusTemperature = temp.main.temp - 273;
-            let tempData = parseFloat(celciusTemperature).toFixed(2);
+    navigator.geolocation.getCurrentPosition(async (location) => {
+      latitudeData = location.coords.latitude;
+      longitudeData = location.coords.longitude;
+      locationArray.push(latitudeData);
+      locationArray.push(longitudeData);
 
-            document.getElementById("temp").innerHTML = tempData + "°c";
+      const data = await apiData(locationArray);
+      console.log("-----apidata-----", data);
+      document.getElementById("icon").src = "loader.gif";
+      iconCode = data.weather[0].icon;
+      document.getElementById("icon").src = `icons/${iconCode}.png`;
+      let celciusTemperature = data.main.temp - 273;
+      let tempData = parseFloat(celciusTemperature).toFixed(2);
 
-            document.getElementById("description").innerHTML =
-              temp.weather[0].description;
+      document.getElementById("temp").innerHTML = tempData + "°c";
 
-            document.getElementById("city-country").innerHTML =
-              temp.name + " - " + temp.sys.country;
+      document.getElementById("description").innerHTML =
+        data.weather[0].description;
 
-            document.getElementById("description").innerHTML =
-              temp.weather[0].description;
-          })
-          .catch((err) => console.log(err));
-      },
-      (err) => {
-        console.warn("unable to fetch");
-      }
-    );
+      document.getElementById("city-country").innerHTML =
+        data.name + " - " + data.sys.country;
+
+      document.getElementById("description").innerHTML =
+        data.weather[0].description;
+    });
   } else {
     console.log("not find the if part");
   }
-}
+};
 getCordinates();
